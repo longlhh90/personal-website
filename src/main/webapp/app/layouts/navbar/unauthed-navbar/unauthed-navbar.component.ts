4619e64ccf0a6c4ad6bfdf999e33bacf0df30265
Lukas Le <longlhh90@google.com>
2021-07-26 22:22:23 -0600

    add unauthed-navbar for public view

---
+++ src/main/webapp/app/layouts/navbar/unauthed-navbar/unauthed-navbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'jhi-unauthed-navbar',
+  templateUrl: './unauthed-navbar.component.html',
+  styleUrls: ['./unauthed-navbar.component.scss'],
+})
+export class UnauthedNavbarComponent implements OnDestroy {
+  isHomeUrl = false;
+  isMobileNavClicked = false;
+
+  private routeUrlSubscription: Subscription;
+
+  constructor(private router: Router) {
+    this.routeUrlSubscription = this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomeUrl = this.router.url === '/';
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeUrlSubscription.unsubscribe();
+  }
+
+  onMobileNav() {
+    this.isMobileNavClicked = !this.isMobileNavClicked;
+  }
+}
