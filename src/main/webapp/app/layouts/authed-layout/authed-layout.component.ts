7b20129199c95e4cf6718058ffe86ded56ff2b2d
Lukas Le <longlhh90@google.com>
2021-07-26 22:33:16 -0600

    reformat and add authed-layout

---
+++ src/main/webapp/app/layouts/authed-layout/authed-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-authed-layout',
+  templateUrl: './authed-layout.component.html',
+  styleUrls: ['./authed-layout.component.scss'],
+})
+export class AuthedLayoutComponent {}
