0d312ab56ca9fbb1639550a1b052f679a4e0d048
Lukas Le <longlhh90@google.com>
2021-07-26 22:33:35 -0600

    add authed-layout to app.module

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -28,6 +28,9 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { UnauthedNavbarComponent } from './layouts/navbar/unauthed-navbar/unauthed-navbar.component';
+import { UnauthedLayoutComponent } from './layouts/unauthed-layout/unauthed-layout.component';
+import { AuthedLayoutComponent } from './layouts/authed-layout/authed-layout.component';
 
 @NgModule({
   imports: [
@@ -59,7 +62,17 @@ import { ErrorComponent } from './layouts/error/error.component';
     { provide: NgbDateAdapter, useClass: NgbDateDayjsAdapter },
     httpInterceptorProviders,
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  declarations: [
+    MainComponent,
+    NavbarComponent,
+    UnauthedNavbarComponent,
+    ErrorComponent,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    FooterComponent,
+    UnauthedLayoutComponent,
+    AuthedLayoutComponent,
+  ],
   bootstrap: [MainComponent],
 })
 export class AppModule {
