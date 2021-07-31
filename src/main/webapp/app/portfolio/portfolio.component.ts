49d71e0a4f2673a80b234ff5e4435f2a57116e40
Lukas Le <longlhh90@google.com>
2021-07-31 13:17:23 -0600

    move porfolio to maintaining

--- src/main/webapp/app/portfolio/portfolio.component.ts
+++ src/main/webapp/app/portfolio/portfolio.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IMe } from 'app/entities/me/me.model';
 import { MeService } from 'app/entities/me/service/me.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'jhi-portfolio',
@@ -10,11 +8,9 @@ import { Observable } from 'rxjs';
   styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
-  meInfo$: Observable<IMe[] | null> | undefined;
-
   constructor(private meService: MeService, private router: Router) {}
 
   ngOnInit(): void {
-    this.meInfo$ = this.meService.query();
+    const test = null;
   }
 }
